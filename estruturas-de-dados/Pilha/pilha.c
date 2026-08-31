b4e9a6e5a0355e15002bf9744a7f36f61c4009ee
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-08-31 09:41:14 -0300

    Add int static stack module

--- estruturas-de-dados/Pilha/pilha.c
+++ estruturas-de-dados/Pilha/pilha.c
@@ -1,12 +1,10 @@
 #include<stdlib.h>
-#include<stdio.h>
-#define MAX 4
+#include"pilha.h"
 
 struct pilha{ // stack
     int dados[MAX];
     int topo;
 };
-typedef struct pilha* Pilha;
 
 Pilha criar(){
     Pilha p = malloc(sizeof(struct pilha));
@@ -43,20 +41,4 @@ int desempilhar(Pilha p){
 void destruir(Pilha p) {
     if(p != NULL)
         free(p);
-}
-
-int main(){
-    Pilha p = criar();
-    empilhar(p, 50);
-    empilhar(p, 60);
-    empilhar(p, 70);
-    empilhar(p, 20);
-    printf("Topo da pilha: %d\n", acessar_topo(p));
-    desempilhar(p);
-    desempilhar(p);
-    empilhar(p, 80);
-    empilhar(p, 100);
-    empilhar(p, 200);
-    printf("Topo da pilha: %d\n", acessar_topo(p));
-    destruir(p);
 }
---
+++ estruturas-de-dados/Pilha/pilha.h
@@ -0,0 +1,14 @@
+#define MAX 4
+
+typedef struct pilha* Pilha;
+
+//create
+Pilha criar();
+// push
+int empilhar(Pilha p, int valor);
+//peek
+int acessar_topo(Pilha p);
+// pop
+int desempilhar(Pilha p);
+// free
+void destruir(Pilha p);

896bb4ccaeb7d4ce60587526e83b5329a8475371
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-09-21 07:55:28 -0300

    Update dynamic int list

--- estruturas-de-dados/Lista/Lista_Unicamente_Encadeada/lista_int.c
+++ estruturas-de-dados/Lista/Lista_Unicamente_Encadeada/lista_int.c
@@ -53,11 +53,55 @@ int inserir_final(Lista li, int valor_inserir){
     }
     return 0;
 }
-
 // remover_inicio
-// remover_final
+int remover_inicio(Lista li){
+    if(li->qtd == 0) return 0;
+    Elem* aux = li->inicio;
+    li->inicio = aux->prox;
+    free(aux);
+    li->qtd--;
+    return 1;
+}
 // acessar_inicio
+int acessar_inicio(Lista li){
+    if(li->qtd == 0) return 0;
+    return li->inicio->valor;
+}
+// destruir
+void destruir(Lista li){
+    if(li == NULL) return;
+    Elem* aux = li->inicio;
+    while(aux->prox != NULL){
+        Elem* atual = aux;
+        aux = aux->prox;
+        free(atual);
+    }
+    free(aux);
+    free(li);
+}
+// remover_final
+int remover_final(Lista li){
+    if(li->qtd == 0) return 0;
+    Elem* aux = li->inicio;
+    if(aux->prox == NULL){
+        free(aux);
+        li->inicio = NULL;
+        li->qtd--;
+        return 1;
+    }
+    Elem* ant;
+    while(aux->prox != NULL){
+        ant = aux;
+        aux = aux->prox;
+    }
+    free(aux);
+    ant->prox = NULL;
+    li->qtd--;
+    return 1;
+}
+
+
+
 // acessar_final
 // buscar_por_valor
 // buscar_por_posicao
-// destruir
